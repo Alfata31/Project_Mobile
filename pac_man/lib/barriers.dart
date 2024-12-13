707dc8f29f7d0ee3d8ff16a53b433fe9edcc7412
tuanrasyid <richardonewgate@gmail.com>
2024-12-13 09:42:57 +0700

    update barrier

--- pac_man/lib/barriers.dart
+++ pac_man/lib/barriers.dart
@@ -1,23 +1,33 @@
 import 'package:flutter/material.dart';
 
 class MyBarrier extends StatelessWidget {
-  final innerColor;
-  final outerColor;
-  final child;
+  final Color innerColor;
+  final Color outerColor;
+  final Widget child;
 
-  MyBarrier({this.innerColor, this.outerColor, this.child});
+  const MyBarrier({
+    Key? key,
+    required this.innerColor,
+    required this.outerColor,
+    required this.child,
+  }) : super(key: key);
+
+  static const double outerPadding = 1.0;
+  static const double outerBorderRadius = 6.0;
+  static const double outerContainerPadding = 12.0;
+  static const double innerBorderRadius = 10.0;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(1.0),
+      padding: const EdgeInsets.all(outerPadding),
       child: ClipRRect(
-        borderRadius: BorderRadius.circular(6),
+        borderRadius: BorderRadius.circular(outerBorderRadius),
         child: Container(
-          padding: EdgeInsets.all(12),
+          padding: const EdgeInsets.all(outerContainerPadding),
           color: outerColor,
           child: ClipRRect(
-            borderRadius: BorderRadius.circular(10),
+            borderRadius: BorderRadius.circular(innerBorderRadius),
             child: Container(
               color: innerColor,
               child: child,
